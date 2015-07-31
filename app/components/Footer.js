a5d7f64267bd75f98edeedde944e2291d7e9acb3
Ogawa Mitsuru <mitsuru.ogawa@flect.co.jp>
2015-07-31 21:01:12 +0900

    Create Footer

---
+++ app/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router';
+import FooterStore from './stores/FooterStore';
+import FooterActions  from './stores/FooterActions';
+
+class Footer extends React.Component {
+
+  constructor(props) {
+    supre(props);
+  }
+  
+  render() {
+  }
+  
+  export default Footer;
+  
+}
